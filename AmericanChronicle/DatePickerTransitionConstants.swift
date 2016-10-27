36d554e49599d84331e6c4e4b2e238a412d25811
Ryan Peterson <ryanipete@gmail.com>
2016-10-26 21:22:05 -0600

    Fix compiler warning

--- AmericanChronicle/DatePickerTransitionConstants.swift
+++ AmericanChronicle/DatePickerTransitionConstants.swift
@@ -1,13 +1,3 @@
-//
-//  DatePickerTransitionConstants.swift
-//  AmericanChronicle
-//
-//  Created by Ryan Peterson on 10/26/16.
-//  Copyright © 2016 ryanipete. All rights reserved.
-//
-
-import Foundation
-
 struct DatePickerTransitionConstants {
     static let snapshotTag = 1001
 }
